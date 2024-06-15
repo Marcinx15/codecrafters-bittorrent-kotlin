ab6ec55570160f27e89eaf036bafb0911ff84d92
Marcin Kurek <marcin.kurek@rocker.com>
2024-06-15 02:52:32 +0200

    decode strings

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,26 @@
-import com.google.gson.Gson;
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
-val gson = Gson()
-
 fun main(args: Array<String>) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println("Logs from your program will appear here!")
-    val command = args[0]
-    when (command) {
+
+    when (val command = args[0]) {
         "decode" -> {
-            // Uncomment this block to pass the first stage
-            // val bencodedValue = args[1]
-            // val decoded = decodeBencode(bencodedValue)
-            // println(gson.toJson(decoded))
-            // return
+            val bencodedValue = args[1]
+            val decoded = decodeBencode(bencodedValue)
+            println(Json.encodeToString(decoded))
         }
         else -> println("Unknown command $command")
     }
 }
 
-fun decodeBencode(bencodedString: String): String {
+fun decodeBencode(bencodedString: String): String =
     when {
-        Character.isDigit(bencodedString[0]) -> {
+        bencodedString.first().isDigit() -> {
             val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
-            val length = Integer.parseInt(bencodedString.substring(0, firstColonIndex))
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
+            val length = bencodedString.take(firstColonIndex).toInt()
+            bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
         }
         else -> TODO("Only strings are supported at the moment")
     }
-}
+
