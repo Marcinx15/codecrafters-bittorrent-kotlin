fff5daa9c8468b4ac396ef4323679839f42d45a6
Marcin Kurek <marcin.kurek@rocker.com>
2024-06-15 18:23:30 +0200

    decode integers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 fun main(args: Array<String>) {
@@ -8,7 +10,7 @@ fun main(args: Array<String>) {
         "decode" -> {
             val bencodedValue = args[1]
             val decoded = decodeBencode(bencodedValue)
-            println(Json.encodeToString(decoded))
+            println(decoded)
         }
         else -> println("Unknown command $command")
     }
@@ -19,8 +21,22 @@ fun decodeBencode(bencodedString: String): String =
         bencodedString.first().isDigit() -> {
             val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
             val length = bencodedString.take(firstColonIndex).toInt()
-            bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
+            Json.encodeToString(bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length))
         }
-        else -> TODO("Only strings are supported at the moment")
+
+        bencodedString.first() == 'i' -> {
+            val numberSubstring = bencodedString.drop(1).takeWhile { it != 'e' }
+            if (numberSubstring.length > 1 && numberSubstring[0] == '0') {
+                throw InvalidFormatException
+            } else if (numberSubstring.length >= 2 && numberSubstring[0] == '-' && numberSubstring[1] == '0') {
+                throw InvalidFormatException
+            } else {
+                Json.encodeToString(numberSubstring.toLong())
+            }
+        }
+
+        else -> TODO()
     }
+
+object InvalidFormatException : Exception()
 
